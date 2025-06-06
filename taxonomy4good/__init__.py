b5872a0fe19ef6fc0576edd4b8e66edcf544ec87
rjmlaird <rjmlaird@gmail.com>
2025-06-06 19:59:00 +0100

    🔄 Sync changes

--- taxonomy4good/__init__.py
+++
@@ -1,4 +0,0 @@
-from .sustainabilityItem import SustainabilityItem
-from .sustainabilityTaxonomy import SustainabilityTaxonomy, from_file
-from .errors import EmptyTaxonomyError, IDNotFoundError, FileTypeNotSupportedError
-
